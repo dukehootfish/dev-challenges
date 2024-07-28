2ceedecfd600ecc4aa1979fe60f9de0901a65945
dukehootfish <fishelad01@gmail.com>
2024-07-28 05:34:25 +0300

    Add initial TypeScript configuration and sample code
    - Added `tsconfig.json` with TypeScript compiler options.
    - Created VSCode launch configuration in `launch.json`.
    - Added sample TypeScript code in `src/index.ts`.
    - Compiled output and source map in `dist/`.

---
+++ Learning TypeScript/dist/index.js
@@ -0,0 +1,6 @@
+"use strict";
+var age = 25;
+if (age >= 18) {
+    console.log('You are an adult');
+}
+//# sourceMappingURL=index.js.map
---
+++ Learning TypeScript/src/index.ts
@@ -0,0 +1,4 @@
+var age: number = 25;
+if (age >= 18) {
+    console.log('You are an adult');
+}
